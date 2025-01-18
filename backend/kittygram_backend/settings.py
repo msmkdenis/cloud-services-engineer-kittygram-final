370307f2ea30fbf4bd9297fab1f8d0f666495c99
Denis Burtsev <d.burtsev@postgrespro.ru>
2025-01-18 16:48:01 +0300

    fix: nginx conf

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -93,7 +93,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/backend_static/static/'
+STATIC_ROOT = '/backend_static/collected_static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
