15cd177f4d73cfbdd34ca31704cfea3c3652fdc4
Denis Burtsev <d.burtsev@postgrespro.ru>
2025-04-30 18:19:04 +0300

    fix: vpc network

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -17,17 +17,19 @@ resource "yandex_vpc_security_group" "infra_sg" {
   egress {
     protocol       = "ANY"
     v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
   }
 
   ingress {
     protocol       = "TCP"
-    port           = "22"
+    port           = 8000
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     protocol       = "TCP"
-    port           = "80"
+    port           = 22
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
