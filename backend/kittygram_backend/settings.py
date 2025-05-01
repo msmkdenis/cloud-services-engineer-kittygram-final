fd566b83578ba153fbba2dc3427ad37058908ddf
Denis Burtsev <d.burtsev@postgrespro.ru>
2025-05-01 15:27:57 +0300

    Revert "fix: static"
    This reverts commit 4a8ba92515aa9c2d29f0a94096f097c3f8b047c1.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -93,7 +93,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/backend_static/'  # Корень volume для статики
+STATIC_ROOT = '/backend_static/collected_static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/media_static/'
