4a8ba92515aa9c2d29f0a94096f097c3f8b047c1
Denis Burtsev <d.burtsev@postgrespro.ru>
2025-05-01 15:20:27 +0300

    fix: static

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -93,7 +93,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/backend_static/collected_static/'
+STATIC_ROOT = '/backend_static/'  # Корень volume для статики
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/media_static/'
