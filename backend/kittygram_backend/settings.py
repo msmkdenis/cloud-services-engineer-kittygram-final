5069ea9d334225e67eff9ff6b6a1c00fc41fbd3e
Denis Burtsev <d.burtsev@postgrespro.ru>
2025-05-01 21:15:31 +0300

    Revert "fix: static"
    This reverts commit caa7bcc2165cf89ab4a6c1b60da74695e048d9ed.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -92,10 +92,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static') 
+STATIC_ROOT = '/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media_static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
