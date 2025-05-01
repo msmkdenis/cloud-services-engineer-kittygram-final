caa7bcc2165cf89ab4a6c1b60da74695e048d9ed
Denis Burtsev <d.burtsev@postgrespro.ru>
2025-05-01 20:26:31 +0300

    fix: static

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -92,10 +92,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static') 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media_static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
